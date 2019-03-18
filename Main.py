c082237307ec391df45cb357e2cdfdd3c86e042a
talathom <tp22tala@siena.edu>
2019-03-18 13:07:35 -0400

    First Commit

---
+++ Main.py
@@ -0,0 +1,63 @@
+﻿import vizcam
+from Controller import *
+
+window = viz.MainWindow
+viz.window.setSize(640, 480)
+pivotNav = vizcam.PivotNavigate()
+view = viz.MainView
+mat = viz.Matrix()
+mat.postTrans(100, 1950, 100)
+view.setMatrix(mat)
+
+inputFile = open('greysriverOrig.asc', 'r')
+firstLine = inputFile.readline()
+NCols = int(firstLine.split()[1])
+secondLine = inputFile.readline()
+NRows = int(secondLine.split()[1])
+arr = list()
+
+for i in range(0, 4):
+	inputFile.readline()
+vertices = inputFile.readlines()
+
+vertIndex = 0
+for i in range(0, NRows):
+	arr.append(vertices[i].split())
+
+max = arr[0][0]
+min = arr[0][0]
+for i  in range(0, NRows):
+	for j in range(0, NCols):
+		arr[i][j] = float(arr[i][j])
+		if arr[i][j] > max:
+			max = arr[i][j]
+		if arr[i][j] < min:
+			min = arr[i][j]
+
+print(str(NRows))
+print(str(NCols))
+print(str(max))
+print(str(min))
+
+viz.startLayer(viz.TRIANGLES)
+for z in range(0, NRows-1): 
+	for x in range(0, NCols-1):
+		viz.vertex(x, arr[z][x], z)
+		viz.vertex(x, arr[z+1][x], z+1)
+		viz.vertex(x+1, arr[z][x+1], z)
+		viz.vertex(x+1, arr[z][x+1], z)
+		viz.vertex(x, arr[z+1][x], z+1)
+		viz.vertex(x+1, arr[z+1][x+1], z+1)
+		if arr[z][x] < 1896:
+			viz.vertexColor(0, 0, 1)
+		elif arr[z][x] < 1950:
+			viz.vertexColor(0,0.25, 0)
+		elif arr[z][x] < 2000:
+			viz.vertexColor(0,0.5, 0)
+		elif arr[z][x] < 2050:
+			viz.vertexColor(0,.75, 0)
+		else:
+			viz.vertexColor(0, 1, 0)
+viz.endLayer()
+
+viz.go()
