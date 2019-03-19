6c59f6e66c29eeeaec2d071bfe00d3b176ab8cb3
talathom <tp22tala@siena.edu>
2019-03-19 12:17:42 -0400

    Fixed Min/Max Bug

--- Main.py
+++ Main.py
@@ -1,30 +1,34 @@
 ﻿import vizcam
 
 window = viz.MainWindow
-viz.window.setSize(640, 480)
-pivotNav = vizcam.PivotNavigate()
+viz.window.setSize(640, 480) # Set Window Size
+pivotNav = vizcam.PivotNavigate() # Turn on Pilot Navigation
 view = viz.MainView
 mat = viz.Matrix()
-mat.postTrans(100, 1950, 100)
+mat.postTrans(100, 1950, 100) # Move the Camera to a Position where the terrain is visible
 view.setMatrix(mat)
 
-inputFile = open('greysriver.asc', 'r')
+inputFile = open('greysriver.asc', 'r') # Open the file
+#Read in the number of rows and columns
 firstLine = inputFile.readline()
 NCols = int(firstLine.split()[1])
 secondLine = inputFile.readline()
 NRows = int(secondLine.split()[1])
 arr = list()
 
+# Consume irrelevant lines
 for i in range(0, 4):
 	inputFile.readline()
+# Read in all remaining lines
 vertices = inputFile.readlines()
 
 vertIndex = 0
 for i in range(0, NRows):
 	arr.append(vertices[i].split())
 
-max = arr[0][0]
-min = arr[0][0]
+max = float(arr[0][0])
+min = float(arr[0][0])
+#Find max and min elevation
 for i  in range(0, NRows):
 	for j in range(0, NCols):
 		arr[i][j] = float(arr[i][j])
@@ -32,12 +36,13 @@ for i  in range(0, NRows):
 			max = arr[i][j]
 		if arr[i][j] < min:
 			min = arr[i][j]
+# Output info
+print("Rows "+ str(NRows))
+print("Columns "+ str(NCols))
+print("Max "+ str(max))
+print("Min "+ str(min))
 
-print(str(NRows))
-print(str(NCols))
-print(str(max))
-print(str(min))
-
+# Generate Terrain
 viz.startLayer(viz.TRIANGLES)
 for z in range(0, NRows-1): 
 	for x in range(0, NCols-1):
